eee68cf005b87f22d6f692adabab41d254d2adc8
Juliette Rousselet <juliette.roussleet@etu.univ-orleans.fr>
2023-10-24 09:08:39 +0200

    création dans views.py des pages html prévues

--- ConcertPro/views.py
+++ ConcertPro/views.py
@@ -6,3 +6,33 @@ def accueil():
     return render_template(
         "accueil.html"
     )
+
+@app.route('/creer_concert')
+def creer_concert():
+    return render_template(
+        "creer_concert.html"
+    )
+
+@app.route('/voir_prochains_concerts')
+def voir_prochains_concerts():
+    return render_template(
+        "voir_prochains_concerts.html"
+    )
+
+@app.route('/ajout_nouvelle_salle')
+def ajout_nouvelle_salle():
+    return render_template(
+        "ajout_nouvelle_salle.html"
+    )
+
+@app.route('/voir_salles')
+def voir_salles():
+    return render_template(
+        "voir_salles.html"
+    )
+
+@app.route('/historique_concert')
+def historique_concert():
+    return render_template(
+        "historique_concert.html"
+    )
