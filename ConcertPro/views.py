4bf35ccee15a5f5b581db5d06de2b74cae90147c
Juliette Rousselet <juliette.roussleet@etu.univ-orleans.fr>
2023-10-24 10:55:26 +0200

    ajout du contenu du formulaire de creer_concert

--- ConcertPro/views.py
+++ ConcertPro/views.py
@@ -1,6 +1,9 @@
 from flask import render_template
 from .app import app
 
+ARTISTES = [{"nom": "Muse"}, {"nom": "Daft Punk"}]
+CONCERTS = [{"artiste": "Muse", "date": "2019-01-01", "salle": "Bercy", "url":"test", "nom":"concert 1"}, {"artiste": "Daft Punk", "date": "2019-01-01", "salle": "Bercy", "url":"test", "nom":"concert 1"}]
+
 @app.route('/')
 def accueil():
     return render_template(
@@ -10,14 +13,15 @@ def accueil():
 @app.route('/creer_concert')
 def creer_concert():
     return render_template(
-        "creer_concert.html"
+        "creer_concert.html",
+        artistes=ARTISTES
     )
 
 @app.route('/voir_prochains_concerts')
 def voir_prochains_concerts():
     return render_template(
         "voir_prochains_concerts.html",
-        conerts=[]
+        concerts=CONCERTS
     )
 
 @app.route('/ajout_nouvelle_salle')
