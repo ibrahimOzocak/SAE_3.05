5d09ab993a1023a2cc5d9b1d38e72e3af86ead7e
BRISSETLo <leo.brisset@etu.univ-orleans.fr>
2023-11-06 10:13:03 +0100

    Ajout des insertions et test de la connexion à la bd

--- ConcertPro/models.py
+++ ConcertPro/models.py
@@ -5,3 +5,16 @@ db = mysql.connector.connect(
 )
 
 cursor = db.cursor()
+
+def test():
+    try:
+        req1 = "SELECT * FROM Artiste"
+        cursor.execute(req1)
+        info = cursor.fetchall()
+        for i in info:
+            print(i)
+    except Exception as e:
+        print(e.args)
+        
+if __name__ == "__main__":
+    test()
--- insert.sql
+++ insert.sql
@@ -1,84 +1,75 @@
-INSERT INTO Artiste (id_artiste, telephone, mail, nom_artiste, prenom_artiste, date_de_naissance, lieu_naissance, adresse, securite_social, conge_spectacle, cni, date_delivrance_cni, date_experation_cni, carte_reduction, id_style_musique, plan_feu)
-VALUES
-('1', '0123456789', 'artiste1@example.com', 'Artiste1', 'Prénom1', '1990-01-01', 'Ville1', 'Adresse1', '1234567890', 'Congé1', 'CNI123', '2022-01-01', '2025-01-01', 'Carte1', '1', 'PlanFeu1'),
-('2', '9876543210', 'artiste2@example.com', 'Artiste2', 'Prénom2', '1991-02-02', 'Ville2', 'Adresse2', '0987654321', 'Congé2', 'CNI456', '2022-02-02', '2025-02-02', 'Carte2', '2', 'PlanFeu2'),
-('3', '1112223333', 'artiste3@example.com', 'Artiste3', 'Prénom3', '1992-03-03', 'Ville3', 'Adresse3', '1112223333', 'Congé3', 'CNI789', '2022-03-03', '2025-03-03', 'Carte3', '3', 'PlanFeu3');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Avoir (id_salle, id_concert, plan_feu, installation, FOH, backline, retour)
-VALUES
-('1', '1', 'PlanFeu1', 'Installation1', 'FOH1', 'Backline1', 'Retour1'),
-('2', '2', 'PlanFeu2', 'Installation2', 'FOH2', 'Backline2', 'Retour2'),
-('3', '3', 'PlanFeu3', 'Installation3', 'FOH3', 'Backline3', 'Retour3');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Besoin_equipement_artiste (id_concert, id_artiste, id_equipement, quantite, possede_equipement)
-VALUES
-('1', '1', '1', '5', 'Oui'),
-('2', '2', '2', '3', 'Non'),
-('3', '3', '3', '7', 'Oui'),
--- Ajoutez 7 autres lignes ici
-INSERT INTO Concert (id_concert, nom_concert, date_heure_concert, duree_concert, id_artiste, id_salle, photo)
-VALUES
-('1', 'Concert1', '2023-10-27 20:00:00', '120', '1', '1', 'photo1.jpg'),
-('2', 'Concert2', '2023-10-28 19:30:00', '90', '2', '2', 'photo2.jpg'),
-('3', 'Concert3', '2023-10-29 21:15:00', '150', '3', '3', 'photo3.jpg');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Equipement (id_equipement, nom_equipement)
-VALUES
-('1', 'Microphone'),
-('2', 'Guitare'),
-('3', 'Batterie');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Equipement_Dispo_Salle (id_salle, id_equipement)
-VALUES
-('1', '1'),
-('2', '2'),
-('3', '3');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Jouer (id_artiste, id_style_musique)
-VALUES
-('1', '1'),
-('2', '2'),
-('3', '3');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Logement (id_logement, adresse_ville_codepostal, nom_etablissement, nb_etoile)
-VALUES
-('1', 'Adresse1', 'Etablissement1', '4'),
-('2', 'Adresse2', 'Etablissement2', '3'),
-('3', 'Adresse3', 'Etablissement3', '5');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Loger (id_artiste, id_logement, id_concert, nb_personne, nb_nuit)
-VALUES
-('1', '1', '1', '2', '5'),
-('2', '2', '2', '3', '4'),
-('3', '3', '3', '1', '7');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Participer (id_concert, id_artiste, transport, restauration)
-VALUES
-('1', '1', 'Transport1', 'Restauration1'),
-('2', '2', 'Transport2', 'Restauration2'),
-('3', '3', 'Transport3', 'Restauration3');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Posseder (id_salle, id_equipement)
-VALUES
-('1', '1'),
-('2', '2'),
-('3', '3');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Salle (id_salle, id_type_salle, loge, nom_salle, nb_places, profondeur_scene, longeur_scene, description_salle, adresse_salle, telephone_salle, id_salle_2)
-VALUES
-('1', '1', 'Loge1', 'Salle1', '1000', '5', '10', 'Description1', 'AdresseSalle1', '1234567890', '1'),
-('2', '2', 'Loge2', 'Salle2', '800', '4', '8', 'Description2', 'AdresseSalle2', '9876543210', '2'),
-('3', '3', 'Loge3', 'Salle3', '1200', '6', '12', 'Description3', 'AdresseSalle3', '1112223333', '3');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Style_musique (id_style_musique, nom_style_musique)
-VALUES
-('1', 'Rock'),
-('2', 'Pop'),
-('3', 'Classique');
--- Ajoutez 7 autres lignes ici
-INSERT INTO Type_Salle (id_type, type_place_s, acceuil_pmr, id_salle)
-VALUES
-('1', 'TypePlace1', 'Oui', '1'),
-('2', 'TypePlace2', 'Non', '2'),
-('3', 'TypePlace3', 'Oui', '3');
--- Ajoutez 7 autres lignes ici
+-- Insertion dans la table Style_musique
+INSERT INTO Style_musique (id_style_musique, nom_style_musique) VALUES
+(1, 'Rock'),
+(2, 'Pop'),
+(3, 'Hip-Hop'),
+(4, 'Jazz');
+
+-- Insertion dans la table Equipement
+INSERT INTO Equipement (id_equipement, nom_equipement) VALUES
+(1, 'Microphone'),
+(2, 'Enceinte'),
+(3, 'Batterie'),
+(4, 'Guitare électrique');
+
+-- Insertion dans la table Logement
+INSERT INTO Logement (id_logement, adresse_ville_codepostal, nom_etablissement, nb_etoile) VALUES
+(1, 'Paris 75001', 'Hôtel Parisien', 3),
+(2, 'Marseille 13001', 'Auberge Provençale', 2),
+(3, 'Lyon 69001', 'Grand Hôtel Lyonnais', 4);
+
+-- Insertion dans la table Artiste
+INSERT INTO Artiste (id_artiste, telephone, mail, nom_artiste, prenom_artiste, date_de_naissance, lieu_naissance, adresse, securite_social, conge_spectacle, cni, date_delivrance_cni, date_expiration_cni, carte_reduction, id_style_musique) VALUES
+(1, '555-123-4567', 'artiste1@example.com', 'John', 'Doe', '1990-05-15', 'New York', '123 Main St', '123-45-6789', 'Oui', 'ABC123456', '2020-01-15', '2025-01-15', 'Réduction1', 1),
+(2, '555-987-6543', 'artiste2@example.com', 'Jane', 'Smith', '1985-08-20', 'Los Angeles', '456 Elm St', '987-65-4321', 'Non', 'XYZ987654', '2019-06-10', '2024-06-10', 'Réduction2', 2);
+
+-- Insertion dans la table Jouer
+INSERT INTO Jouer (id_artiste, id_style_musique) VALUES
+(1, 1),
+(1, 2),
+(2, 3);
+
+-- Insertion dans la table Type_Salle
+INSERT INTO Type_Salle (id_type, type_place_s, accueil_pmr) VALUES
+(1, 'Salle de concert', true),
+(2, 'Théâtre', false),
+(3, 'Salle polyvalente', true);
+
+-- Insertion dans la table Salle
+INSERT INTO Salle (id_salle, id_type_salle, loge, nom_salle, nb_places, profondeur_scene, longueur_scene, description_salle, adresse_salle, telephone_salle) VALUES
+(1, 1, 'Oui', 'Salle de Concert A', 500, 10, 20, 'Salle de concert moderne', '123 Main St, Paris', '555-111-2222'),
+(2, 2, 'Non', 'Théâtre B', 300, 8, 15, 'Théâtre classique', '456 Elm St, Marseille', '555-333-4444');
+
+-- Insertion dans la table Concert
+INSERT INTO Concert (id_concert, nom_concert, date_heure_concert, duree_concert, id_artiste, id_salle, photo) VALUES
+(1, 'Concert Rock', '2023-11-15 20:00:00', 120, 1, 1, NULL),
+(2, 'Concert Pop', '2023-12-10 19:30:00', 90, 2, 2, NULL);
+
+-- Insertion dans la table Avoir
+INSERT INTO Avoir (id_salle, id_concert, plan_feu, installation, FOH, backline, retour) VALUES
+(1, 1, 'Plan Feu A', 'Installation A', 'FOH A', 'Backline A', 'Retour A'),
+(2, 2, 'Plan Feu B', 'Installation B', 'FOH B', 'Backline B', 'Retour B');
+
+-- Insertion dans la table Posseder
+INSERT INTO Posseder (id_salle, id_equipement, quantite) VALUES
+(1, 1, '10 microphones'),
+(1, 2, '8 enceintes'),
+(2, 3, '1 batterie'),
+(2, 4, '5 guitares électriques');
+
+-- Insertion dans la table Besoin_equipement_artiste
+INSERT INTO Besoin_equipement_artiste (id_concert, id_artiste, id_equipement, quantite, possede_equipement) VALUES
+(1, 1, 1, '5 microphones', 'Oui'),
+(1, 1, 2, '4 enceintes', 'Oui'),
+(2, 2, 3, '1 batterie', 'Non'),
+(2, 2, 4, '2 guitares électriques', 'Oui');
+
+-- Insertion dans la table Loger
+INSERT INTO Loger (id_artiste, id_logement, id_concert, nb_personne, nb_nuit) VALUES
+(1, 1, 1, 2, 3),
+(2, 2, 2, 1, 2);
+
+-- Insertion dans la table Participer
+INSERT INTO Participer (id_concert, id_artiste, transport, restauration) VALUES
+(1, 1, 'Voiture', 'Oui'),
+(2, 2, 'Avion', 'Non');
