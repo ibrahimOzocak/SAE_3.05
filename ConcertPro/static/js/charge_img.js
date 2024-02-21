05c5999ad15dff4eca11b9b56a0371577d1ddcac
ibrahimOzocak <123069336+ibrahimOzocak@users.noreply.github.com>
2024-02-21 15:26:20 +0100

    Revert "#47 css rider"

--- ConcertPro/static/js/charge_img.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('image-input').addEventListener('change', function() {
-    var fichier = this.files[0]; // Obtenir le premier fichier sélectionné
-    if (fichier) {
-        var img = document.getElementById('image-charge');
-        img.src = URL.createObjectURL(fichier);
-    }
-});
